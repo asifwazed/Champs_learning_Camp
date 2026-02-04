d0ec5a0d6043233c4dd67e27d1646751201817fc
asifwazed <asifwazed78@gmail.com>
2026-02-05 02:50:59 +0600

    Create vocab.js with initial vocabulary list
    Initialized vocabulary database with words and meanings.

---
+++ vocab.js
@@ -0,0 +1,12 @@
+// Vocabulary Database
+// Add new words here to update the Flashcards automatically!
+const vocabList = [
+    { w: "Apartheid", m: "বর্ণবৈষম্য", s: "Discrimination" },
+    { w: "Emancipation", m: "মুক্তি", s: "Freedom" },
+    { w: "Reconciliation", m: "পুনর্মিলন", s: "Reunion" },
+    { w: "Sophisticated", m: "জটিল/আধুনিক", s: "Complex" },
+    { w: "Stuttering", m: "তোতলানো", s: "Falters" },
+    { w: "Adulteration", m: "ভেজাল", s: "Contamination" },
+    { w: "Erratic", m: "অনিয়মিত", s: "Unpredictable" },
+    { w: "Plight", m: "দুর্দশা", s: "Suffering" }
+];
