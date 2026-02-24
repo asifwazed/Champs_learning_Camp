c0f6cdff62e25849e25d0df48f31320d89d05340
asifwazed <asifwazed78@gmail.com>
2026-02-24 16:33:30 +0600

    Create exam-db.js

---
+++ exam-db.js
@@ -0,0 +1,35 @@
+const examDB = {
+    // ==========================================
+    // EXAMS FOR UNIT 1
+    // ==========================================
+    "u1_l1": {
+        mcqs: [
+            { q: "What does the word 'apartheid' refer to in the text?", options: ["Apartment", "Distance", "Racial discrimination", "Dialogue"], ans: 2 },
+            { q: "Nelson Mandela was imprisoned for nearly...", options: ["Two decades", "Three decades", "Four decades", "Ten years"], ans: 1 },
+            { q: "The word 'shackles' stands for...", options: ["Freedom", "Chains", "Laws", "Punishments"], ans: 1 },
+            { q: "When did Mandela become South Africa's first black president?", options: ["1990", "1993", "1994", "2004"], ans: 2 },
+            { q: "What was the clan name by which friends fondly called Mandela?", options: ["Madiba", "Rivonia", "Klerk", "Gordimer"], ans: 0 }
+        ],
+        openEnded: [
+            "Why was Nelson Mandela imprisoned?",
+            "What did Mandela say about race discrimination in his acceptance speech?",
+            "Why was the Nobel Peace Prize awarded to both Mandela and F.W. de Klerk?"
+        ],
+        flowChartTopic: "the life and achievements of Nelson Mandela.",
+        flowChartBox1: "Guiding South Africa to democracy"
+    },
+    
+    "u1_l2": {
+        mcqs: [
+            { q: "Where was Valentina Tereshkova born?", options: ["Moscow", "Maslennikovo", "Karnal", "Texas"], ans: 1 },
+            { q: "Kalpana Chawla obtained her Ph.D. in Aerospace Engineering from:", options: ["Punjab University", "University of Texas", "University of Colorado", "Zhukovsky Academy"], ans: 2 }
+        ],
+        openEnded: [
+            "How did Valentina Tereshkova prepare for her space flight?"
+        ],
+        flowChartTopic: "the life and achievements of Kalpana Chawla.",
+        flowChartBox1: "Born in Karnal, India"
+    }
+
+    // You will just keep pasting all future exams in here!
+};
